a52c91fbb5ff656f6c5143692278857e3c64684c
(EvstEgor) <(egorevst4567@gmail.com)>
2024-11-19 12:25:43 +0300

    Added class Exam.cs v2

--- TeamProject/Exam.cs
+++ TeamProject/Exam.cs
@@ -4,9 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeamProject
+namespace TeamProject;
+
+internal class Exam
 {
-    internal class Exam
+    public string Subject { get; set; }
+    public int Grade { get; set; }
+    public DateTime ExamDate { get; set; }
+
+    public Exam(string subject, int grade, DateTime examDate)
     {
+        Subject = subject;
+        Grade = grade;
+        ExamDate = examDate;
+    }
+
+    public Exam()
+    {
+        Subject = "предмет";
+        Grade = 0;
+        ExamDate = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return $"Предмет: {Subject}, Оценка: {Grade}, Дата экзамена: {ExamDate.ToShortDateString()}";
     }
 }
